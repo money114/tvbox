ab915dbd50a2c7a39620a79cec38ba04e7d69ffa
money114 <57452664+money114@users.noreply.github.com>
2025-11-10 22:19:14 +0800

    Update 51吸瓜.py

--- py/51吸瓜.py
+++ py/51吸瓜.py
@@ -21,7 +21,7 @@ from base.spider import Spider
 class Spider(Spider):
 
     def init(self, extend="{}"):
-        self.domin='https://www.vwnyhjhc.cc'
+        self.domin='https://www.awyckfe.cc'
         self.proxies = {}
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
@@ -405,6 +405,7 @@ class Spider(Spider):
         decrypted = unpad(cipher.decrypt(word), AES.block_size)
 
         return decrypted
+
 
 
 
